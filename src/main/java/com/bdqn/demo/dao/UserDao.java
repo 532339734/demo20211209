41e42358ec134e7b0bdb5ff96372ea4c424e225f
曾攀辉 <532339734@qq.com>
2021-12-09 14:19:56 +0800

    first

---
+++ src/main/java/com/bdqn/demo/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.bdqn.demo.dao;
+
+/**
+ * description TODO
+ *
+ * @author zph
+ * date 2021/12/9 14:16
+ */
+
+public class UserDao {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
